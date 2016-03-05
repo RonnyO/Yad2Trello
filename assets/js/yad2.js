21271fe08b3f247138e46afcb2085592ab351c5e
Ronny Orbach <ronny@reallygood.co.il>
2016-03-05 22:57:14 +0200

    Fix scope and refactor

--- assets/js/yad2.js
+++ assets/js/yad2.js
@@ -1,15 +1,15 @@
 // abuse the note cells to inject Open In New Tab buttons
-$('[id^=ad_note_2]');.each(function() {
+$('[id^=ad_note_]').each(function() {
     var $el = $(this).empty();
 
     var link = $('<a>').attr({
-        href: $el.parent().children().last().find('a').attr('href'),
+        href: $el.siblings().last().find('a').attr('href'),
         target: '_blank',
-        class: 'yad2trello-newtab'
+        class: 'yad2trello-newtab',
+        title: 'פתיחת מודעה בלשונית חדשה'
     });
     var img = $('<img>').attr({
-        src: chrome.extension.getURL('assets/images/newtab.png'),
-        title:"פתיחת מודעה בלשונית חדשה"
+        src: chrome.extension.getURL('assets/images/newtab.png')
     });
 
     link.append(img).appendTo($el);
